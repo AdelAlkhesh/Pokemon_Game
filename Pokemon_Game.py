1da4e3342153570661895690a119b3818a8df72c
Shen <splinteredsanity@outlook.com>
2021-08-13 18:56:53 +0200

    Edited functions for better readability.

--- Pokemon_Game.py
+++ Pokemon_Game.py
@@ -1,58 +1,50 @@
-class Pokemon(): 
-    
-
-   
-   def __init__(self, name, level, type,max_health, curr_health):
-     
-       self.name = name 
-       self.level = level 
-       self.type = type 
-       self.max_health = max_health
-       self.curr_health = curr_health
 
 
-   def lose_health(self,damage): 
-       self.curr_health -= damage
-       print("Your pokemon lost {amount} health".format( amount = damage))
-       print("Your pokemon is now at {amount} HP".format(amount = self.curr_health))
-       if self.curr_health <= 0: 
-           self.knock_out()
-       return self.curr_health
-  
-    
-   def regain_health(self, heal):
-       if self.curr_health != self.max_health:
+class Pokemon():
+
+    def __init__(self, name, level, type, max_health, curr_health):
+
+        self.name = name
+        self.level = 1
+        self.type = type
+        self.max_health = max_health
+        self.curr_health = curr_health
+
+    def lose_health(self, damage):
+        hit_counter = 0
+        self.curr_health -= damage
+        print("{} lost {} health".format(self.name, damage))
+        print("{} is now at {} HP".format(self.name, self.curr_health))
+        if self.curr_health <= 0:
+            self.knock_out()
+        return self.curr_health
+
+    def regain_health(self, heal):
+        if self.curr_health != self.max_health:
             self.curr_health += heal
-            print("Your Pokemon regained {} HP".format(heal))
-            print("Your pokemon is now at {} HP".format(self.curr_health))
+            print("{} regained {} HP".format(self.name, heal))
+            print("{} is now at {} HP".format(self.name, self.curr_health))
             return self.curr_health
-       else: 
-           print("Your Pokemon is already at Max HP!")
+        else:
+            print("Your Pokemon is already at Max HP!")
 
-   def knock_out(self):
+    def knock_out(self):
 
-       print("Oh no! Your Pokemon has been knocked the fuck out!")
+        print("{} has been knocked out!".format(self.name))
 
+    def revive(self):
+        self.curr_health = self.max_health / 2
+        print("{} has been revived with half HP!".format(self.name))
+        print("{} has {} HP".format(self.name, self.curr_health))
 
-
-   def revive(self):
-       self.curr_health = self.max_health / 2
-       print("Your Pokemon has been revived with half HP!")
-       print("Your Pokemon has {} HP".format(self.curr_health))
-       
-
-
-
-shen = Pokemon("Shen", 12, "Fire", 400, 400)
-shen.lose_health(30)
-shen.regain_health(20)
-shen.knock_out()
-shen.revive()
-shen.lose_health(200)
-    
+    def level_up(self):
+        self.level += 1
+        self.max_health *= 1.5 * self.level
+        self.curr_health = self.max_health
+        print("{} levels up and is now level {}!".format(self.name, self.level))
+        print("{} now has {} Maximum HP!".format(self.name, self.max_health))
 
 
-   
 
 
-    
+
